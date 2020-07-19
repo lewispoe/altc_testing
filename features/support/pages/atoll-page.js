f6e4a319f993b1981a8955eee9137a5163ea0ba5
lewispoe <lewis.poe@gmail.com>
2020-07-19 18:57:33 +0100

    Cleaning up other code

--- features/support/pages/atoll-page.js
+++ features/support/pages/atoll-page.js
@@ -2,13 +2,6 @@ const {Selector} = require('testcafe');
 
 // Selectors
 const select = selector => Selector(selector).with({boundTestRun: testController})
-const selectWithExactText = (selector, text) => Selector(selector).withExactText(text).with({boundTestRun: testController})
-
-const findAutocompleteChooser = async() => {
-    const labelForInput = await findAnElementByTextContent('label', 'Enter station name');
-    return labelForInput.parent().child('div').child('input');
-}
-
 
 exports.elems = {
     url: () =>'http://localhost:3000/',
@@ -23,26 +16,4 @@ const selectButton = async (text) => {
     };
     let list = await mapIt();
     return getElement(list, text, 'button');
-}
-
-function getElement(list, textContent, elementType) {
-    const find = list.find(x => x.text === textContent);
-    if (find) {
-        console.log("I found the element for: <" + elementType + '> and content: ' + textContent);
-        return find.element.with({boundTestRun: testController});
-    } else {
-        console.log("Failed to find the element for: <" + elementType + '> and content: ' + textContent);
-        console.log(list.map(x => x.text));
-    }
-    throw "Unable to find element <" + elementType + ">,  with text: " + textContent;
-}
-
-async function findAnElementByTextContent(elementType, textContent) {
-    const parentContainer = Selector(elementType).with({boundTestRun: testController});
-    const mapDataByText = async () => {
-        const map = new Array(await parentContainer.count).fill('').map(async (x, idx) => {const element = await parentContainer.nth(idx);return {text: await element.textContent, element: element}});
-        return await Promise.all(map);
-    };
-    let list = await mapDataByText();
-    return getElement(list, textContent, elementType);
 }
