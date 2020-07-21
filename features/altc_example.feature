bb2663227732b2b7324a9d00f80a428bbd6b51c1
Chiaro-K <mapumak@gmail.com>
2020-07-21 12:45:14 +0200

    proper stations selected test passed

--- features/altc_example.feature
+++ features/altc_example.feature
@@ -1,6 +1,31 @@
 Feature: Test out capabilities
 
-  Scenario: Load the app page
+  Background: Starting on home page
     Given I am on the example home page
 
+  # Scenario: Load the app page
+  #   Then I should see "Penzance Trains..."
+
   #Start adding the tests....
+  Scenario: Search for available tickets
+    When I select "Penzance" on the from-station dropdown
+    And I select "Redruth" on the to-station dropdown
+    And I click the submit button on the Home page
+    Then the results table should exist
+
+  # Scenario: Search for available tickets without selecting arrival point
+  #   When I select "Penzance" on the from-station dropdown
+  #   And I click the "Find Times And Tickets" button
+  #   Then the "arrival" error should display
+
+  # Scenario: Search for available tickets without selecting departure point
+  #   When I select "Redruth" on the to-station dropdown
+  #   And I click the "Find Times And Tickets" button
+  #   Then the "departure" error should display
+
+  # Scenario: Search for available tickets for the same departure and destination
+  #   When I select "Redruth" on the from-departure dropdown
+  #   And I select "Redruth" on the to-departure dropdown
+  #   And I click the "Find Times And Tickets" button
+  #   Then the "same" error should appear
+    
--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,9 +1,16 @@
+// const fs = require('fs');
+// const createTestCafe = require('testcafe');
+const testControllerHolder = require('../support/testControllerHolder');
+// const { AfterAll, setDefaultTimeout, Before, After, Status } = require('cucumber');
+// const errorHandling = require('../support/errorHandling');
+// const TIMEOUT = 20000;
+
 const fs = require('fs');
 const createTestCafe = require('testcafe');
-const testControllerHolder = require('../support/testControllerHolder');
-const {AfterAll, setDefaultTimeout, Before, After, Status} = require('cucumber');
-const errorHandling = require('../support/errorHandling');
-const TIMEOUT = 20000;
+// const testControllerHolder = require('./testControllerHolder');
+const {AfterAll, setDefaultTimeout, Before, After} = require('cucumber');
+const timeout = 100000;
+// let cafeRunner = null;
 
 let isTestCafeError = false;
 let attachScreenshotToReport = null;
@@ -11,86 +18,68 @@ let cafeRunner = null;
 let n = 0;
 
 function createTestFile() {
-    fs.writeFileSync('test.js',
-        'import errorHandling from "./features/support/errorHandling.js";\n' +
-        'import testControllerHolder from "./features/support/testControllerHolder.js";\n\n' +
-
-        'fixture("fixture")\n' +
-
-        'test\n' +
-        '("test", testControllerHolder.capture)')
-}
-
-function runTest(iteration, browser) {
-    createTestCafe('localhost', 1338 + iteration, 1339 + iteration)
-        .then(function(tc) {
-            cafeRunner = tc;
-            const runner = tc.createRunner();
-            return runner
-                .src('./test.js')
-                .screenshots('reports/screenshots/', true)
-                .browsers(browser)
-                .run()
-                .catch(function(error) {
-                    console.error(error);
-                });
-        })
-        .then(function(report) {
+        fs.writeFileSync('cucumbertest.js',
+            'import testControllerHolder from "./features/support/testControllerHolder.js";\n\n' +
+            'fixture("cucumberfixture")\n' +
+            'test\n' +
+            '("test", testControllerHolder.capture)')
+    }
+    // Create a runner function with configurations like src, screenshots, browsers.
+    function runTest(browser) {
+        createTestCafe('localhost', 1337, 1338)
+            .then(function(tc) {
+                cafeRunner = tc;
+                const runner = tc.createRunner();
+                return runner
+                    .src('./cucumbertest.js')
+                    .screenshots('reports/screenshots/', true)
+                    .browsers(browser)
+                    .run();
+            });
+    }
+    // Setting the default time out value
+    setDefaultTimeout(timeout);
+    /*
+    50
+    1. Before hook runs before each Cucumber test.
+    51
+    2. It calls the "runTest" function, which contains the runner configuration.
+    52
+    3. Then, it calls the "createTestFile" function. It generates a dummy file, cucumbertest.js, behaving as the source of the tests.
+    53
+    4. Then, it calls the waitForTestController of cucumberWorld.js to add testController to Cucumber’s world scope.
+    54
+    5. Then, it also maximizes the test controller window.
+    55
+    */
+    Before(function() {
+        runTest('chrome');
+        createTestFile();
+        return this.waitForTestController.then(function(testController) {
+            return testController.maximizeWindow();
         });
-}
-
-
-setDefaultTimeout(TIMEOUT);
-
-Before(function() {
-    runTest(n, this.setBrowser());
-    createTestFile();
-    n += 2;
-    return this.waitForTestController.then(function(testController) {
-        return testController.maximizeWindow();
     });
-});
-
-After(function() {
-    fs.unlinkSync('test.js');
-    testControllerHolder.free();
-});
-
-After(async function(testCase) {
-    const world = this;
-    if (testCase.result.status === Status.FAILED) {
-        isTestCafeError = true;
-        attachScreenshotToReport = world.attachScreenshotToReport;
-        errorHandling.addErrorToController();
-        await errorHandling.ifErrorTakeScreenshot(testController)
-    }
-});
-
-AfterAll(function() {
-    let intervalId = null;
-
-    function waitForTestCafe() {
-        intervalId = setInterval(checkLastResponse, 500);
-    }
-
-    function checkLastResponse() {
-        if (testController.testRun.lastDriverStatusResponse === 'test-done-confirmation') {
-            cafeRunner.close();
-            process.exit();
-            clearInterval(intervalId);
+    // After hook runs after each Cucumber test. It is used to unlink the test and make testController "null".
+    // It calls the testControllerHolder.free function. 
+    After(function() {
+        fs.unlinkSync('cucumbertest.js');
+        testControllerHolder.free();
+    });
+    // AfterAll hook runs after all the tests execution. It check the last runs status to be "test-done-confirmation",
+    // and then, close the cafeRunner and exit the process.
+    // It checks with a wait timeout of 500.
+    AfterAll(function() {
+        let intervalId = null;
+        function waitForTestCafe() {
+            intervalId = setInterval(checkLastResponse, 500);
         }
-    }
-
-    waitForTestCafe();
-});
-
-const getIsTestCafeError = function() {
-    return isTestCafeError;
-};
-
-const getAttachScreenshotToReport = function(path) {
-    return attachScreenshotToReport(path);
-};
-
-exports.getIsTestCafeError = getIsTestCafeError;
-exports.getAttachScreenshotToReport = getAttachScreenshotToReport;
+        function checkLastResponse() {
+            if (testController.testRun.lastDriverStatusResponse === 'test-done-confirmation') {
+                cafeRunner.close();
+                process.exit();
+            }
+        }
+        waitForTestCafe();
+    });
+// exports.getIsTestCafeError = getIsTestCafeError;
+// exports.getAttachScreenshotToReport = getAttachScreenshotToReport;
