bc536bcbd86a7bb12aca01b9f79a03b73b608617
Chiaro-K <mapumak@gmail.com>
2020-07-21 13:26:57 +0200

    proper stations selected test passed

--- features/altc_example.feature
+++ features/altc_example.feature
@@ -13,10 +13,10 @@ Feature: Test out capabilities
     And I click the submit button on the Home page
     Then the results table should exist
 
-  # Scenario: Search for available tickets without selecting arrival point
-  #   When I select "Penzance" on the from-station dropdown
-  #   And I click the "Find Times And Tickets" button
-  #   Then the "arrival" error should display
+  Scenario: Search for available tickets without selecting arrival point
+    When I select "Penzance" on the from-station dropdown
+    And I click the "Find Times And Tickets" button
+    Then the error "Error: Arrival Station Not Set" should display
 
   # Scenario: Search for available tickets without selecting departure point
   #   When I select "Redruth" on the to-station dropdown
--- features/step_definitions/altc.js
+++ features/step_definitions/altc.js
@@ -2,10 +2,10 @@ const { Given, When, Then } = require('cucumber');
 const atollPage = require('../support/pages/atoll-page');
 const { Selector } = require('testcafe');
 
-Given(/^I am on the example home page$/, async () => {
-    const url = atollPage.elems.url();
-    await testController.navigateTo(url);
-});
+// Given(/^I am on the example home page$/, async () => {
+//     const url = atollPage.elems.url();
+//     await testController.navigateTo(url);
+// });
 
 // Then('I should see {string}', async function (string) {
 //     const main = Selector('header').with({ boundTestRun: testController })
@@ -14,35 +14,35 @@ Given(/^I am on the example home page$/, async () => {
 // });
 
 
-When('I select {string} on the from-station dropdown', async function (string) {
-    const fromSelect = Selector('#from-station').with({ boundTestRun: testController });
-    const fromOption = fromSelect.find('option').with({ boundTestRun: testController });
+// When('I select {string} on the from-station dropdown', async function (string) {
+//     const fromSelect = Selector('#from-station').with({ boundTestRun: testController });
+//     const fromOption = fromSelect.find('option').with({ boundTestRun: testController });
 
-    await testController
-        .click(fromSelect)
-        .click(fromOption.withText(string))
+//     await testController
+//         .click(fromSelect)
+//         .click(fromOption.withText(string))
 
-});
+// });
 
-When('I select {string} on the to-station dropdown', async function (string) {
-    const toSelect = Selector('#to-station').with({ boundTestRun: testController });
-    const toOption = toSelect.find('option').with({ boundTestRun: testController });
+// When('I select {string} on the to-station dropdown', async function (string) {
+//     const toSelect = Selector('#to-station').with({ boundTestRun: testController });
+//     const toOption = toSelect.find('option').with({ boundTestRun: testController });
 
-    await testController
-        .click(toSelect)
-        .click(toOption.withText(string))
+//     await testController
+//         .click(toSelect)
+//         .click(toOption.withText(string))
 
-});
+// });
 
-When('I click the submit button on the Home page', async function () {
-    const submitButton = Selector('.penca-button').with({ boundTestRun: testController });
+// When('I click the submit button on the Home page', async function () {
+//     const submitButton = Selector('.penca-button').with({ boundTestRun: testController });
 
-    await testController.click(submitButton)
-});
+//     await testController.click(submitButton)
+// });
 
-Then('the results table should exist', async function () {
-    let main = Selector('.main').with({ boundTestRun: testController })
-    let results = main.find('.results').with({ boundTestRun: testController })
+// Then('the results table should exist', async function () {
+//     let main = Selector('.main').with({ boundTestRun: testController })
+//     let results = main.find('.results').with({ boundTestRun: testController })
 
-    await results.exists;
-});
+//     await results.exists;
+// });
