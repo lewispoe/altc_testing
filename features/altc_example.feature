3f43fa26d4094755f3179cb4aa3b84cfb568046d
Chiaro-K <mapumak@gmail.com>
2020-07-21 13:49:54 +0200

    not selecting destination test passes

--- features/altc_example.feature
+++ features/altc_example.feature
@@ -7,25 +7,25 @@ Feature: Test out capabilities
   #   Then I should see "Penzance Trains..."
 
   #Start adding the tests....
-  Scenario: Search for available tickets
-    When I select "Penzance" on the from-station dropdown
-    And I select "Redruth" on the to-station dropdown
-    And I click the submit button on the Home page
-    Then the results table should exist
+  # Scenario: Search for available tickets
+  #   When I select "Penzance" on the from-station dropdown
+  #   And I select "Redruth" on the to-station dropdown
+  #   And I click the submit button on the Home page
+  #   Then the results table should exist
 
   Scenario: Search for available tickets without selecting arrival point
     When I select "Penzance" on the from-station dropdown
-    And I click the "Find Times And Tickets" button
+    And I click the submit button on the Home page
     Then the error "Error: Arrival Station Not Set" should display
 
   # Scenario: Search for available tickets without selecting departure point
   #   When I select "Redruth" on the to-station dropdown
   #   And I click the "Find Times And Tickets" button
-  #   Then the "departure" error should display
+  # Then the error "Error: Arrival Station Not Set" should display
 
   # Scenario: Search for available tickets for the same departure and destination
   #   When I select "Redruth" on the from-departure dropdown
   #   And I select "Redruth" on the to-departure dropdown
   #   And I click the "Find Times And Tickets" button
-  #   Then the "same" error should appear
+  # Then the error "Error: Arrival Station Not Set" should display
     
--- features/step_definitions/altc.js
+++ features/step_definitions/altc.js
@@ -2,47 +2,54 @@ const { Given, When, Then } = require('cucumber');
 const atollPage = require('../support/pages/atoll-page');
 const { Selector } = require('testcafe');
 
-// Given(/^I am on the example home page$/, async () => {
-//     const url = atollPage.elems.url();
-//     await testController.navigateTo(url);
-// });
+Given(/^I am on the example home page$/, async () => {
+    const url = atollPage.elems.url();
+    await testController.navigateTo(url);
+});
 
-// Then('I should see {string}', async function (string) {
-//     const main = Selector('header').with({ boundTestRun: testController })
+Then('I should see {string}', async function (string) {
+    const main = Selector('header').with({ boundTestRun: testController })
 
-//     await testController.expect(main.child('p').innerText).contains(string)
-// });
+    await testController.expect(main.child('p').innerText).contains(string)
+});
 
 
-// When('I select {string} on the from-station dropdown', async function (string) {
-//     const fromSelect = Selector('#from-station').with({ boundTestRun: testController });
-//     const fromOption = fromSelect.find('option').with({ boundTestRun: testController });
+When('I select {string} on the from-station dropdown', async function (string) {
+    const fromSelect = Selector('#from-station').with({ boundTestRun: testController });
+    const fromOption = fromSelect.find('option').with({ boundTestRun: testController });
 
-//     await testController
-//         .click(fromSelect)
-//         .click(fromOption.withText(string))
+    await testController
+        .click(fromSelect)
+        .click(fromOption.withText(string))
 
-// });
+});
 
-// When('I select {string} on the to-station dropdown', async function (string) {
-//     const toSelect = Selector('#to-station').with({ boundTestRun: testController });
-//     const toOption = toSelect.find('option').with({ boundTestRun: testController });
+When('I select {string} on the to-station dropdown', async function (string) {
+    const toSelect = Selector('#to-station').with({ boundTestRun: testController });
+    const toOption = toSelect.find('option').with({ boundTestRun: testController });
 
-//     await testController
-//         .click(toSelect)
-//         .click(toOption.withText(string))
+    await testController
+        .click(toSelect)
+        .click(toOption.withText(string))
 
-// });
+});
 
-// When('I click the submit button on the Home page', async function () {
-//     const submitButton = Selector('.penca-button').with({ boundTestRun: testController });
+When('I click the submit button on the Home page', async function () {
+    const submitButton = Selector('.penca-button').with({ boundTestRun: testController });
 
-//     await testController.click(submitButton)
-// });
+    await testController.click(submitButton)
+});
 
-// Then('the results table should exist', async function () {
-//     let main = Selector('.main').with({ boundTestRun: testController })
-//     let results = main.find('.results').with({ boundTestRun: testController })
+Then('the results table should exist', async function () {
+    let results = Selector('.results').with({ boundTestRun: testController })
+    let table = results.find('table').with({ boundTestRun: testController })
 
-//     await results.exists;
-// });
+    await table.exists;
+});
+
+Then('the error {string} should display', async function (error) {
+    let results = Selector('.results').with({ boundTestRun: testController })
+    let err = results.find('h2').with({ boundTestRun: testController })
+
+    await testController.expect(err.innerText).contains(error);
+});
